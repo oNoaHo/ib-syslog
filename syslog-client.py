052c7a0c166e968892aeeca2ea74fab14f7dbc6a
NoaH <github@13floor.de>
2020-05-03 20:23:53 +0200

    init

---
+++ syslog-client.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import socket
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 1514        # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    while True:
+        s.connect((HOST, PORT))
+        send_data = input("Type some text to send => ")
+        s.sendall(b''+send_data.encode('utf-8'))
+        # print("\n\n 1. Server sent : ", send_data, "\n\n")
+        # s.sendall(b'Hello, world')
+        # data = s.recv(1024)
+
+print('Received', repr(data))
---
+++ syslog-srv.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import socket
+import sys
+
+if len(sys.argv) == 3:
+    # Get "IP address of Server" and also the "port number" from argument 1 and argument 2
+    ip = sys.argv[1]
+    port = int(sys.argv[2])
+else:
+    print("Run like : python3 server.py <arg1:server ip:this system IP 192.168.1.6> <arg2:server port:4444 >")
+    exit(1)
+
+# Create a UDP socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Bind the socket to the port
+server_address = (ip, port)
+s.bind(server_address)
+print("Do Ctrl+c to exit the program !!")
+print("####### Server is listening #######")
+
+while True:
+    data, address = s.recvfrom(4096)
+    print("Server received: ", data.decode('utf-8'))
+
+
+# import socket
+
+# HOST = '0.0.0.0'  # Standard loopback interface address (localhost)
+# PORT = 1514        # Port to listen on (non-privileged ports are > 1023)
+
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.bind((HOST, PORT))
+#     s.listen()
+#     conn, addr = s.accept()
+#     with conn:
+#         print('Connected by', addr)
+#         while True:
+#             data = conn.recv(1024)
+#             if not data:
+#                 break
+#             conn.sendall(b'received')
+#             print(data)
